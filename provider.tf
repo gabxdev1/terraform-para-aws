0658c17b8d7d6e5281493ac0f0f17cea139842b1
gabxdev1 <gabriel.duraes@alunos.unis.edu.br>
2025-10-18 14:03:06 -0300

    add S3 backend configuration for Terraform

--- provider.tf
+++ provider.tf
@@ -1,13 +1,19 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.17.0"
     }
+  }
+
+  backend "s3" {
+    bucket = "comunidadedevops-gab"
+    key    = "dev/terraform.tfstate"
+    region = "sa-east-1"
   }
 }
 
 provider "aws" {
-  region = "sa-east-1"
+  region  = "sa-east-1"
   profile = "automacao-curso"
 }
