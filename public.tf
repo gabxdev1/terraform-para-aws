510deba68af97885379c30103968b641f4be3c81
gabxdev1 <gabriel.duraes@alunos.unis.edu.br>
2025-10-18 16:01:13 -0300

    refactor public subnets to use dynamic availability zones and enhanced tagging

--- public.tf
+++ public.tf
@@ -1,21 +1,27 @@
 resource "aws_subnet" "eks_subnet_public_sa-east-1a" {
   vpc_id                  = aws_vpc.eks-vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
-  availability_zone       = "sa-east-1a"
+  availability_zone       = "${data.aws_region.current.region}a"
   map_public_ip_on_launch = true
-
-  tags = {
-    Name = "comunidadedevops-subnet-public-sa-east-1a"
-  }
+  tags = merge(
+    local.tags,
+    {
+      Name                     = "comunidadedevops-subnet-public-sa-east-1a",
+      "kubernetes.io/role/elb" = "1"
+    }
+  )
 }
 
 resource "aws_subnet" "eks_subnet_public_sa-east-1b" {
   vpc_id                  = aws_vpc.eks-vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
-  availability_zone       = "sa-east-1b"
+  availability_zone       = "${data.aws_region.current.region}b"
   map_public_ip_on_launch = true
-
-  tags = {
-    Name = "comunidadedevops-subnet-public-sa-east-1b"
-  }
+  tags = merge(
+    local.tags,
+    {
+      Name                     = "comunidadedevops-subnet-public-sa-east-1b",
+      "kubernetes.io/role/elb" = "1"
+    }
+  )
 }
