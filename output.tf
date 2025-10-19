6a4e5a735541728fa18381c1264b804030d1ce01
gabxdev1 <gabriel.duraes@alunos.unis.edu.br>
2025-10-19 18:28:37 -0300

    feat: add output for EKS cluster CA certificate in output.tf

--- output.tf
+++ output.tf
@@ -7,3 +7,8 @@ output "cluster_endpoint" {
   value       = module.eks_cluster.cluster_endpoint
   description = "EKS Cluster Endpoint"
 }
+
+output "cluster_ca_certificate" {
+  value       = module.eks_cluster.cluster_ca_certificate
+  description = "EKS Cluster CA Certificate"
+}
